cdb7025542a20acf9d2c3367de7f9cd43fa5d3e9
Alhai <alhai38@icloud.com>
2022-11-14 11:51:04 +0100

    install intervention / refacto / object tshirt

---
+++ tshirt-app/app/Models/Tshirt.php
@@ -0,0 +1,14 @@
+<?php
+
+class Tshirt
+{
+    private $size;
+    private $gender;
+
+    public function _construct($pSize, $pGender){
+        $this->size = $pSize;
+        $this->gender = $pGender;
+    }
+
+    
+}
